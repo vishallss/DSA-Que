9c63ae3dde05a3c886a615116de1fe05c3988983
Vishal Shinde <vishals10020@gmail.com>
2026-08-19 16:27:42 +0530

    Add sum of elements in arr Que

---
+++ sum_of_element_arr.java
@@ -0,0 +1,15 @@
+public class sum_of_element_arr{
+   public static int CalaulateSum(int arr[]){
+      int sum = 0;
+      for(int i=0; i<arr.length; i++){
+         sum = sum + arr[i];
+      }
+      return sum;
+   }
+   public static void main (String [] args){
+      int arr[] = {1, 2, 3, 4, 5};
+
+      System.out.println(CalaulateSum(arr));
+      
+   }
+}
